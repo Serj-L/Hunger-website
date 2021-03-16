456f6dd2ca919c3d03c8151de1fc5fa193b72c5b
Serj-L <svlepniakov@gmail.com>
2021-03-16 19:14:13 +0300

    adaptive version (excluding Header and Menu)

--- script/script.js
+++ script/script.js
@@ -1,3 +1,20 @@
+ /* scroll up */
+ var scrbtn = $('.scroll-up-btn');
+
+ $(window).scroll(function() {
+   if ($(window).scrollTop() > 100) {
+     scrbtn.addClass('active');
+   } else {
+     scrbtn.removeClass('active');
+   }
+ });
+
+ scrbtn.on('click', function(e) {
+   e.preventDefault();
+   $('html, body').animate({scrollTop:0}, '100');
+ });
+/* /scroll up */
+
 /* кнопки input (People) */
 $(document).on('click', '.input-down-button', function () {
     let total = $(this).prev().prev();
