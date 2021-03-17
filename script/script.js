2cf9e3c6eea0a359b48f15f2279925eb06beaf52
Serj-L <svlepniakov@gmail.com>
2021-03-17 16:15:39 +0300

    modify Menu navigation on small screens

--- script/script.js
+++ script/script.js
@@ -51,42 +51,59 @@ $(document).ready(function() {
         $(".menu__list--pizza").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--pizza").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
+
     });
     $('.soup').click(function(event) {
         $('.soup').addClass("active")
         $(".menu__list--soup").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--soup").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not(this).parent("li").removeClass("active-span")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
     $('.pasta').click(function(event) {
         $('.pasta').addClass("active")
         $(".menu__list--pasta").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--pasta").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
     $('.desert').click(function(event) {
         $('.desert').addClass("active")
         $(".menu__list--desert").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--desert").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
     $('.wine').click(function(event) {
         $('.wine').addClass("active")
         $(".menu__list--wine").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--wine").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
     $('.beer').click(function(event) {
         $('.beer').addClass("active")
         $(".menu__list--beer").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--beer").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
     $('.drinks').click(function(event) {
         $('.drinks').addClass("active")
         $(".menu__list--drinks").addClass("active")
         $(".menu-nav-item").not($(this)).removeClass("active")
         $(".menu__list").not(".menu__list--drinks").removeClass("active")
+        $(this).parent('li').addClass('active-span');
+        $(".active-span").not($(this).parent("li")).removeClass("active-span")
     });
 });
 /* /menu */
